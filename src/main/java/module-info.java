408eb7f14272b5499a818a08a5eb1c3988221beb
Filip Moreau <filip.moreau@hotmail.com>
2021-09-27 16:20:18 +0200

    Imported the old GameView to the new project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.ai_for_data_science {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.example.ai_for_data_science to javafx.fxml;
